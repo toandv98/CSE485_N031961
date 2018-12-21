f6b9ae64e3846f3df0f9ca07905b4cfdfc96d4df
haohv62 <44923738+haohv62@users.noreply.github.com>
2018-12-22 04:02:33 +0700

    hieern thi album

---
+++ p_album.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nhạc Online</title>
+    <link rel="stylesheet" href="./css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/hover.css">
+    <link rel="stylesheet" href="./css/stylephantrang.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container-fullwidth">
+        <?php
+        session_start();
+        include('./php/header.php');
+        ?>
+
+
+<?php
+    include('php/footer.php');
+?>
