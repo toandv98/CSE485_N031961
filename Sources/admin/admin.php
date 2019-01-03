7fe5220dfd40e575cc3d897d547c3f9a785c5d08
haohv62 <44923738+haohv62@users.noreply.github.com>
2019-01-03 16:18:53 +0700

    update title

--- Sources/admin/admin.php
+++ Sources/admin/admin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Trang Quản Trị Admin</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/dataTables.bootstrap4.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/fix.css">
