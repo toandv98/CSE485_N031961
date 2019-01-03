9c4f1b1be7f9b3771e1c2bdaa0fbf34d971a5664
Đặng Văn Toản <toan98.k10@gmail.com>
2019-01-03 19:45:23 +0700

    Auto stash before merge of "master" and "origin/master"

--- Sources/admin/admin.php
+++ Sources/admin/admin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Trang Quản Trị Admin</title>
+    <title>Document</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/dataTables.bootstrap4.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/fix.css">
